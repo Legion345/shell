62cf852b681d3d28b26dfbb4f2de8ee32793b798
Legion345 <harelasaraf@gmail.com>
2024-12-12 12:51:41 -0800

    builtin exit code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
-        System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        String input;
 
-        System.out.println(input + ": command not found");
+        do {
+
+            System.out.print("$ ");
+            input = scanner.nextLine();
+
+            // Check for the exit command
+            if (input.equals("exit")) {
+                System.out.println("$ exit 0");
+                break;
+            }
+
+            System.out.println(input + ": command not found");
+
+        } while (true);
+
+        scanner.close();
 
     }
 }
