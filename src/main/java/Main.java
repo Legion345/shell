8944b73656b850127a78f67c93b7d144f6134c03
Legion345 <harelasaraf@gmail.com>
2024-12-12 12:40:56 -0800

    handling invalid commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("$ ");
+
         String input = scanner.nextLine();
         System.out.println(input + ": command not found");
 
